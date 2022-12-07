34080b9945369109bbf17292b65f2af31f322a9f
Mark Ritterhoff <markr@squareup.com>
2022-12-06 23:22:30 -0500

    remove random webpage save

--- 2022/day07/solution.py
+++ 2022/day07/solution.py
@@ -1,2 +1,14 @@
-from "./helpers"
+from utils import *
+
+# multiple lines
+f = [x for x in open("input.txt").read().strip().split('\n')]
+# one line
+ff = open("input.txt").read().strip()
+
+
+
+
+
+
+
 
