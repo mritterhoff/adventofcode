624abe350a57b62c8c2c9e44989b5e55793cb93d
Mark Ritterhoff <markr@squareup.com>
2022-12-08 00:35:35 -0500

    2022 day 8

---
+++ 2022/day08/solution.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+f = [x for x in open("input.txt").read().strip().split('\n')]
+trees = np.array([[int(x) for x in ff] for ff in f])
+
+def scoreP2(v, arr):
+	for i, el in enumerate(arr):
+		if el >= v: return i + 1
+	return len(arr)
+
+p1, p2 = 2*trees.shape[0] + 2*trees.shape[1] - 4, 0
+for x in range(1, trees.shape[0]-1):
+	for y in range(1, trees.shape[1]-1):
+		score1 = trees[y][x] > max(trees[y,:x])
+		score1 |= trees[y][x] > max(trees[y,x+1:])
+		score1 |= trees[y][x] > max(trees[:y,x])
+		score1 |= trees[y][x] > max(trees[y+1:,x])
+		p1 += 1 if score1 else 0
+
+		score2 = scoreP2(trees[y][x], list(reversed(trees[y,:x])))
+		score2 *= scoreP2(trees[y][x], trees[y,x+1:])
+		score2 *= scoreP2(trees[y][x], list(reversed(trees[:y,x])))
+		score2 *= scoreP2(trees[y][x], trees[y+1:,x])
+		p2 = max(p2, score2)
+
+print('p1:', p1)
+print('p2:', p2)
