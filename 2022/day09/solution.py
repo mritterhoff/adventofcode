6391711ce129018e1da6624dfd64708f091cc52d
Mark Ritterhoff <markr@squareup.com>
2022-12-09 00:47:55 -0500

    no libs

--- 2022/day09/solution.py
+++ 2022/day09/solution.py
@@ -1,54 +1,48 @@
-import sys 
-sys.path.append('../..')
-from utils import *
-
-f = [x for x in open("input.txt").read().strip().split('\n')]
+f = [[x.split(' ')[0], int(x.split(' ')[1])] for x in open("input.txt").read().strip().split('\n')]
 
 dirs = { 
   'R': (1, 0), 
   'L': (-1, 0),
   'U': (0, 1),
   'D': (0, -1)
-  }
+}
 
-def getDir(v1, v2):
-	return int((v1 - v2)/abs(v1 - v2))
+def sign(v):
+	return 0 if v == 0 else int((v)/abs(v)) 
 
-def solve(count = 2):
-	twh = set()
-	knots = dict()
+def manDist(t1, t2):
+	return abs(t1[0] - t2[0]) + abs(t1[1] - t2[1])
 
-	for x in range(count):
-		knots[x] = (0,0)
+def addTupes(t1, t2):
+	return (t1[0] + t2[0], t1[1] + t2[1])
 
-	for x in f:
-		d, s = x.split(' ')
-		s = int(s)
-		for hstep in range(s):
-			for k in range(count-1):
-				h = knots[k]
-				t = knots[k+1]
+def solve(knotCount):
+	tailWasHere = set()
+	knotCoord = dict([(x, (0,0)) for x in range(knotCount)])
+
+	for d, s in f:
+		for _ in range(s):
+			for k in range(knotCount-1):
+				thisKnot, nextKnot = knotCoord[k], knotCoord[k+1]
 				
 				if k == 0:
-					h = (h[0] + dirs[d][0], h[1] + dirs[d][1])
-					knots[k] = h
+					thisKnot = addTupes(thisKnot, dirs[d])
+					knotCoord[k] = thisKnot
 
-				mandist = abs(h[0] - t[0]) + abs(h[1] - t[1])
-				if mandist >= 2:
-					if h[0] == t[0]:
-						t = (t[0], t[1] + getDir(h[1], t[1]))
-					elif h[1] == t[1]:
-						t = (t[0] + getDir(h[0], t[0]), t[1])
+				if manDist(thisKnot, nextKnot) >= 2:
+					moveX, moveY = 0, 0
+					if thisKnot[0] == nextKnot[0] or thisKnot[1] == nextKnot[1]:
+						moveX = sign(thisKnot[0] - nextKnot[0])
+						moveY = sign(thisKnot[1] - nextKnot[1])
 					else:
-						if mandist > 2:
-							dirx = getDir(h[0], t[0])
-							diry = getDir(h[1], t[1])
-							t = (t[0] + dirx, t[1]+diry)
-				knots[k+1] = t
+						if manDist(thisKnot, nextKnot) > 2:
+							moveX = sign(thisKnot[0] - nextKnot[0])
+							moveY = sign(thisKnot[1] - nextKnot[1])
+					nextKnot = addTupes(nextKnot, (moveX, moveY))
+				
+				knotCoord[k+1] = nextKnot
+				if k == knotCount-2: tailWasHere.add(nextKnot)
+	return len(tailWasHere)
 
-				if k == count-2: twh.add(knots[count-1])
-
-	return len(list(twh))
-
-print('p1',solve())
-print('p2',solve(10))
+print('p1:',solve(2))
+print('p2:',solve(10))
