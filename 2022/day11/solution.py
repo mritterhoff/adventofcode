b52b0e91a06ebd33f23065ea885997f2c6d84816
Mark Ritterhoff <markr@squareup.com>
2022-12-11 16:08:20 -0500

    part 2

--- 2022/day11/solution.py
+++ 2022/day11/solution.py
@@ -1,22 +1,69 @@
 import sys 
 sys.path.append('../..')
 from utils import *
+import inspect
 
 # multiple lines
 f = [x for x in open("input.txt").read().strip().split('\n\n')]
 
-# p1 35
-inspectCount = defaultdict(int)
-monkeys = {}
+# p1 35, p2 clock 2:24t
+
+class N1:
+	def __init__(self, num):
+		self.map = {}
+
+		for c in [17,13,19,23,5,2,7,11,3]:
+			self.map[c] = num % c
+
+	def plus(self, num):
+		for c in self.map.keys():
+			self.map[c] = (self.map[c] + num) % c
+
+	def test(self, num):
+		return self.map[num] == 0
+
+	def mult(self, num):
+		for c in self.map.keys():
+			self.map[c] = (self.map[c] * num) % c
+
+	def multSelf(self):
+		for c in self.map.keys():
+			self.map[c] = (self.map[c] * self.map[c]) % c
+
+
+def doOp(old, op):
+	if op[2] == 'old':
+		return old*old
+	else:
+		if op[1] == '+':
+			return old + op[2]
+		return old * op[2]
+
+def doOp2(old, op):
+	if op[2] == 'old':
+		old.multSelf()
+	else:
+		if op[1] == '+':
+			old.plus(op[2])
+		else:
+			old.mult(op[2])
+
 
 def parseMonkeys(str):
 	global monkeys
+	global part
 	lines = str.split('\n')
 
 	monk = {}
-	monk['list'] = [int(x) for x in lines[1].split(':')[1].split(',')]
+	
+	if part == 'p1':
+		monk['list'] = [int(x) for x in lines[1].split(':')[1].split(',')]
+	else:
+		monk['list'] = [N1(int(x)) for x in lines[1].split(':')[1].split(',')]
 
 	monk['op'] = [x for x in parse('  Operation: new = {} {} {}', lines[2])]
+	if monk['op'][2] != 'old':
+		monk['op'][2] = int(monk['op'][2])
 
 	monk['divTest'] = parse('  Test: divisible by {:d}', lines[3])[0]
 
@@ -25,80 +72,47 @@ def parseMonkeys(str):
 
 	monkeys[len(monkeys)] = monk
 
-def monkInspect(curMonkIdx):
-	monk =  monkeys[curMonkIdx]
-	
-	for el in monk['list']:
-		el = doOp(el, monk['op'])
-		el = el // 3 #p1 only
 
-		if el / monk['divTest'] == el // monk['divTest']:
-			monkeys[monk['divTrue']]['list'].append(el)
-		else:
-			monkeys[monk['divFalse']]['list'].append(el)
+def solve(rounds):
+	global part
+	for monkStr in f: parseMonkeys(monkStr)
 
-		inspectCount[curMonkIdx] += 1
+	for rnd in range(rounds):
+		for mIdx in range(len(monkeys)):
+			monk = monkeys[mIdx]
+			while (len(monk['list']) > 0):
+				el = monk['list'].pop()
+				
+				if part == 'p1':
+					el = doOp(el, monk['op']) // 3
+					testPassed = el % monk['divTest'] == 0
+				else:
+					doOp2(el, monk['op'])
+					testPassed = el.test(monk['divTest'])
 
+				if testPassed:
+					monkeys[monk['divTrue']]['list'].append(el)
+				else:
+					monkeys[monk['divFalse']]['list'].append(el)
 
-	monk['list'] = []
+				inspectCount[mIdx] += 1
 
+	s = list(reversed(sorted(inspectCount.values())))
+	print(part, s[0] * s[1])
 
+worryList = []
 
-def doOp(old, op):
-	if op[2] == 'old':
-		return old*old
-	else:
-		if op[1] == '+':
-			return old + int(op[2])
-		return old * int(op[2])
+inspectCount = defaultdict(int)
+monkeys = {}
+part = 'p1'
+solve(20)
 
+worryList = []
 
-for monkStr in f:
-	parseMonkeys(monkStr)
-
-for rnd in range(0, 20):
-	for mIdx in range(len(monkeys)):
-		monkInspect(mIdx)
-
-
-pp(monkeys)
-s = list(reversed(sorted(inspectCount.values())))
-pp(s[0] * s[1])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+inspectCount = defaultdict(int)
+monkeys = {}
+part = 'p2'
+solve(10000)
 
 
 
