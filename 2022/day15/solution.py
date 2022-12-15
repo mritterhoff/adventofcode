ba5e40a2549ff4b17db94c85718f59bdb4886945
Mark Ritterhoff <markr@squareup.com>
2022-12-15 14:27:33 -0500

    tighten up day15 2022

--- 2022/day15/solution.py
+++ 2022/day15/solution.py
@@ -5,44 +5,33 @@ f = [x for x in open("input.txt").read().strip().split('\n')]
 def manDist(t1, t2):
 	return abs(t1[0] - t2[0]) + abs(t1[1] - t2[1])
 
-sens = []
-dists = []
-notHere = {}
+sensorToDist = {}
+notHere = set()
 for line in f:
 	senX, senY, bX, bY = parse("Sensor at x={:d}, y={:d}: closest beacon is at x={:d}, y={:d}", line)
 	dist = manDist((senX, senY), (bX, bY))
 
 	sen = (senX, senY)
-	dists.append(dist)
-	sens.append(sen)
+	sensorToDist[sen] = dist
 	for x in range(senX-dist-1, senX+dist+1):
 		y = 2000000
-		if manDist((x,y), sen) <= dist:
-			notHere[(x,y)] = 'x'
+		if manDist((x,y), sen) <= dist: notHere.add((x,y))
 
-print('p1:', len(notHere)-1)
+print('p1:', len(notHere)-1) # why -1?
 
 max = 4000000
 def checkDistToSensors(x,y):
-	for i in range(len(sens)):
-		if manDist((x,y), sens[i]) <= dists[i]: return False
+	for sensor,dist in sensorToDist.items():
+		if manDist((x,y), sensor) <= dist: return False
 	return x*4000000+y
 
-bounds = set()
 p2Answer = False
-
-for i in range(0, len(sens)):
-	xdiff,ydiff = dists[i]+1, 0
+for sensor,dist in sensorToDist.items():
+	xdiff,ydiff = dist+1, 0
 	while xdiff >=0 and not p2Answer:
-		sX,sY = sens[i][0], sens[i][1]
-		options = [
-			[sX + xdiff, sY + ydiff],
-			[sX - xdiff, sY + ydiff],
-			[sX + xdiff, sY - ydiff],
-			[sX - xdiff, sY - ydiff]
-		]
-		
-		for x,y in options:
+		for signX,signY in [[1,1], [1,-1], [-1,-1], [1, 1]]:
+			x = sensor[0] + xdiff*signX
+			y = sensor[1] + ydiff*signY
 			if 0<=x<=max and 0<=y<=max:
 				p2Answer = checkDistToSensors(x, y)
 				if p2Answer: break
@@ -51,16 +40,4 @@ for i in range(0, len(sens)):
 	if p2Answer: break
 
 print('p2:', p2Answer)
-
-
-
-
-
-
-
-
-
-
-
-
 
